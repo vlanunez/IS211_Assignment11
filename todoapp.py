b175d1abc25a336a10521722bb9f03d367d2dd7e
Vladimir Nunez <vnunez@holyname.org>
2020-11-14 20:43:08 -0500

    Assigment11

---
+++ todoapp.py
@@ -0,0 +1,41 @@
+from flask import Flask, render_template, request, redirect
+import re
+
+app = Flask(__name__)
+
+todo_items = []
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', todo_item=todo_items)
+
+
+@app.route('/submit', methods=['POST'])
+def submit():
+    task = request.form['Task Name']
+    priority = request.form['Priority']
+    email = request.form['Email Address']
+
+    if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+        return redirect('/')
+    elif not task:
+        return redirect('/')
+    elif priority == 'Priority':
+        return redirect('/')
+    else:
+        todo_items.append((task, priority, email))
+
+    print(todo_items)
+    return redirect('/')
+
+
+@app.route('/clear', methods=['POST'])
+def clear():
+    del todo_items[:]
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run()
+
